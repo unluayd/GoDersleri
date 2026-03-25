6e12a5d2a6ffd6f949ff0183aa4a164745775db1
unluayd <unluayd@gmail.com>
2026-03-25 16:06:24 +0300

    Açıklama satırları eklendi

--- Interface3/main.go
+++ Interface3/main.go
@@ -2,13 +2,17 @@ package main
 
 import "fmt"
 
+// ICoder: hem şifreleme hem çözme yapabilen türlerin uyması gereken sözleşme.
+// Bu iki metodu birden sağlayan her struct, ICoder olarak kullanılabilir.
 type ICoder interface {
 	Encode(value string) string
 	Decode(value string) string
 }
 
+// XCodec: X ailesi kodlama uygulaması (Encode + Decode birlikte).
 type XCodec struct{}
 
+// YCodec: Y ailesi kodlama uygulaması (aynı arayüz, farklı davranış).
 type YCodec struct{}
 
 func (*XCodec) Encode(value string) string {
@@ -32,12 +36,14 @@ func (*YCodec) Decode(value string) string {
 }
 
 func main() {
+	// coder hep ICoder tipinde; içine farklı codec'ler konur (çok biçimlilik).
 	var coder ICoder
 
 	coder = &XCodec{}
 	coder.Encode("123456")
 	coder.Decode("abcdef")
 
+	// Aynı değişkene YCodec atanınca çağrılar Y'nin metotlarına gider.
 	coder = &YCodec{}
 	coder.Encode("123456")
 	coder.Decode("abcdef")
